de42aad6ccbbd5582b7a6d88d5db692f38a84244
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-04-17 00:14:42 +0700

    FIX: Output path

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,10 +8,7 @@ mix.webpackConfig({
   }
 });
 
-//this fixes an API change introduced at //https://github.com/webpack/webpack/issues/4549
-mix.setPublicPath(
-  path.resolve(__dirname, 'dist')
-);
+mix.setPublicPath('dist');
 
 mix.autoload({
   jquery: ['$', 'jQuery', 'window.jQuery'],
